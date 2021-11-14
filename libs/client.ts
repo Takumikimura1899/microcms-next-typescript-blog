01ba1234746e7b72602f14f187ebe862d05f7a8b
Takumikimura141899 <kimura141899@gmail.com>
2021-11-14 21:58:53 +0900

    [change] clientparamsの型定義変更

--- libs/client.ts
+++ libs/client.ts
@@ -3,9 +3,9 @@ import { ClientParams } from 'microcms-js-sdk/dist/cjs/types';
 
 export const client = createClient({
   serviceDomain: 'taku1219',
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY ?? '',
 });
 
-export const key: { headers: { 'X-MICROCMS-API-KEY'?: string } } = {
-  headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY },
+export const key = {
+  headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY ?? '' },
 };
